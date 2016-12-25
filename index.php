fb14216aa221a3ec7368744709f7d74c5d65636b
Michal Paluchowski <Michal.Paluchowski@stepstone.com>
2016-12-25 09:14:38 +0100

    Add page footer

--- index.php
+++ index.php
@@ -23,5 +23,16 @@
 </div>
 </div>
 
+<div id="footer">
+    <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+
+    <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.pl"><img src="//creativecommons.org/images/public/somerights20.png" width="88" height="31" alt="Creative Commons License" /></a>
+
+    <div id="copyright">
+        Poza wyszczególnionymi przypadkami treść tej strony<br />podlega licencji <a rel="external" href="http://creativecommons.org/licenses/by/3.0/deed.pl" class="subfoot">Creative Commons Uznanie Autorstwa 3.0</a>.
+    </div>
+</div>
+<?php wp_footer(); ?>
+
 </body>
 </html>
