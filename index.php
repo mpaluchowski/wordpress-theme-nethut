ded5fcddc644e5be22bf086f673f39e1ef2a7f23
Michal Paluchowski <Michal.Paluchowski@stepstone.com>
2016-12-25 09:07:50 +0100

    Add article listing

--- index.php
+++ index.php
@@ -10,5 +10,18 @@
 
 <body>
 
+<div id="container">
+<div id="listing">
+<?php while ( have_posts() ) : the_post(); ?>
+    <?php the_title( '<h1><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' ); ?>
+    <div class="excerpt"><?php the_content( "" ); ?></div>
+    <div class="articleInfo">
+        <?php echo the_date(); ?>,
+        <?php echo the_tags( "" ); ?>
+    </div>
+<?php endwhile; ?>
+</div>
+</div>
+
 </body>
 </html>
