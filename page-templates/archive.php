2af1149fc666d10ead48d275bcf3322f39c3ac2b
Michal Paluchowski <Michal.Paluchowski@stepstone.com>
2016-12-26 09:39:30 +0100

    Add basic archive template

---
+++ page-templates/archive.php
@@ -0,0 +1,26 @@
+<?php
+/* Template Name: Montlhy Archive */
+get_header();
+?>
+
+<div id="container">
+<div id="listing">
+<?php while ( have_posts() ) : the_post(); ?>
+    <?php the_title( '<h1><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' ); ?>
+    <div class="excerpt"><?php the_content( "" ); ?></div>
+    <div class="articleInfo">
+        <?php echo the_date(); ?>,
+        <?php echo the_tags( "" ); ?>
+    </div>
+<?php endwhile; ?>
+</div>
+</div>
+
+<div id="sidebar">
+    <ul class="rssMenu">
+        <?php wp_get_archives( 'type=monthly' ); ?>
+    </ul>
+    <?php dynamic_sidebar( 'sidebar-right-column' ); ?>
+</div>
+
+<?php get_footer();
