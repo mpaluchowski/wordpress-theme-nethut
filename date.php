4bb3729cc52e82e26ea6e79b85a6354835d50c8a
Michal Paluchowski <Michal.Paluchowski@stepstone.com>
2016-12-26 09:48:22 +0100

    Run query for main page

--- date.php
+++ date.php
@@ -1,11 +1,17 @@
 <?php
 /* Template Name: Montlhy Archive */
 get_header();
+
+if ( is_date() ) {
+    $query = $wp_query;
+} else {
+    $query = new WP_Query( ["post_type" => "post"] );
+}
 ?>
 
 <div id="container">
 <div id="listing">
-<?php while ( have_posts() ) : the_post(); ?>
+<?php while ( $query->have_posts() ) : $query->the_post(); ?>
     <?php the_title( '<h1><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' ); ?>
     <div class="excerpt"><?php the_content( "" ); ?></div>
     <div class="articleInfo">
